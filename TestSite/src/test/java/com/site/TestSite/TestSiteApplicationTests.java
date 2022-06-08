315c1e47aa43646a89be1d04ff9c58a90400dc85
topch <topch@ASUS>
2022-06-08 16:43:22 +0300

    1st

---
+++ TestSite/src/test/java/com/site/TestSite/TestSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.TestSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
